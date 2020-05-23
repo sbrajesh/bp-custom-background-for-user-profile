e37192135e99a47836759406c68362e33587119c
raviousprime <ravivats89@gmail.com>
2020-05-23 12:07:09 +0530

    version update

--- bp-custom-background-for-user-profile.php
+++ bp-custom-background-for-user-profile.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: BP Custom Background for User Profile
- * Version:1.1.0
+ * Version:1.1.1
  * Author: Brajesh Singh
  * Author URI: http://buddydev.com/members/sbrajesh/
  * Plugin URI: http://buddydev.com/plugins/bp-custom-background-for-user-profile/
